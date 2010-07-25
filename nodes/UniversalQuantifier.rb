4b5983e45f000351169ac953938cd6aaf232a225
Michael Ficarra <github@michael.ficarra.me>
2010-07-25 10:08:43 -0400

    use proper names for unversal quantifiers and existential quantifiers

--- nodes/UniversalQuantifier.rb
+++ nodes/UniversalQuantifier.rb
@@ -1,9 +1,9 @@
-class ExistentialRule
+class UniversalQuantifier
 	def initialize(terminals,expression)
 		@terminals = terminals
 		@expression = expression
 	end
 	def to_s
-		'∃' + @terminals.join('') + ' ' + @expression.to_s
+		'∀' + @terminals.join('') + ': ' + @expression.to_s
 	end
 end
