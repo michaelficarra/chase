2d68e685231305a67aebcf4b2cfab27d2f88abf0
Michael Ficarra <github@michael.ficarra.me>
2010-08-02 02:40:39 -0400

    existential quantifiers support multiple predicates

--- nodes/ExistentialQuantifier.rb
+++ nodes/ExistentialQuantifier.rb
@@ -4,6 +4,6 @@ class ExistentialQuantifier
 		@predicate = predicate
 	end
 	def to_s
-		'∃' + @terminals.join('') + ' ' + @predicate.to_s
+		'∃' + @terminals.join('') + ' ' + @predicate.join(' ')
 	end
 end
